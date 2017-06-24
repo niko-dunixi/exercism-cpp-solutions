c93da970d4111cd7fc0a2e8f0740dcf60fcde15a
Paul Baker <paul.baker@wgu.edu>
2017-06-24 05:26:13 -0600

    passes test out of the box

--- series/series_test.cpp
+++ series/series_test.cpp
@@ -15,7 +15,6 @@ BOOST_AUTO_TEST_CASE(short_digits)
     BOOST_TEST(expected == actual, tt::per_element());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(long_digits)
 {
     const vector<int> expected{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -25,6 +24,7 @@ BOOST_AUTO_TEST_CASE(long_digits)
     BOOST_TEST(expected == actual, tt::per_element());
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(keeps_the_digit_order_if_reversed)
 {
     const vector<int> expected{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};

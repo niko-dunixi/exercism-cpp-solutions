2722bd27b0eef49a8cdf78a1ed4e261d87eec6d0
Paul Baker <paul.baker@wgu.edu>
2017-06-24 02:46:45 -0600

    Passes test as is

--- queen-attack/queen_attack_test.cpp
+++ queen-attack/queen_attack_test.cpp
@@ -60,7 +60,6 @@ BOOST_AUTO_TEST_CASE(queens_can_attack_when_they_are_on_the_same_row)
     BOOST_REQUIRE(board.can_attack());
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(queens_can_attack_when_they_are_on_the_same_column)
 {
     const queen_attack::chess_board board{std::make_pair(5, 4), std::make_pair(2, 4)};
@@ -68,6 +67,7 @@ BOOST_AUTO_TEST_CASE(queens_can_attack_when_they_are_on_the_same_column)
     BOOST_REQUIRE(board.can_attack());
 }
 
+#if defined(EXERCISM_RUN_ALL_TESTS)
 BOOST_AUTO_TEST_CASE(queens_can_attack_diagonally)
 {
     const queen_attack::chess_board board{std::make_pair(1, 1), std::make_pair(6, 6)};

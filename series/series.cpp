c6026baf3f4a2afdfeabec6f318090a468cee33f
Paul Baker <paul.baker@wgu.edu>
2017-06-24 06:20:28 -0600

    Got everything working/passing tests for all tests.

--- series/series.cpp
+++ series/series.cpp
@@ -14,25 +14,17 @@ namespace series {
     }
 
     std::vector<std::vector<int>> slice(std::string input_digits, int size) {
+        if (size > input_digits.size()) {
+            throw std::domain_error("You cannot take a slice larger than the sample.");
+        }
         const std::vector<int> &parsed_digits = digits(input_digits);
-        const auto count = (input_digits.length() - size + 1) * size;
-//        std::cout << "count: " << count << std::endl;
         std::vector<std::vector<int>> results;
 
         for (auto i(0); i < (parsed_digits.size() - (size) + 1); ++i) {
             auto begin = std::begin(parsed_digits) + i;
             auto end = std::begin(parsed_digits) + (i + size);
-//            std::cout << "begin: " << *begin << " end: " << *end << std::endl;
             results.emplace_back(std::vector<int>(begin, end));
         }
-//        std::cout << "done" << std::endl << std::endl;
-//        std::for_each(std::begin(results), std::end(results), [](const std::vector<int> &items) {
-//            std::cout << items.size() << ": ";
-//            std::for_each(std::begin(items), std::end(items), [](const int &current_item) {
-//                std::cout << current_item << " ";
-//            });
-//            std::cout << std::endl;
-//        });
         return results;
     }
 }
